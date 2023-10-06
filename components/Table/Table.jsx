6a389256b0234e0c8b21e97f9941fc823a2e6248
priya <113510836+Priyadharshini0612@users.noreply.github.com>
2023-10-06 18:17:30 +0530

    Add files via upload

---
+++ components/Table/Table.jsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "./Table.css";
+
+function createData(name, trackingId, date, status) {
+  return { name, trackingId, date, status };
+}
+
+const rows = [
+  createData("Priyadharshini T", 18908424, "2 Oct 2023", "Accept"),
+  createData("Shakitha banu A", 18908424, "2 Oct 2023", "Denied"),
+  createData("Varsha G", 18908424, "2 Oct 2023", "Accept"),
+  createData("Payaskhan S", 18908421, "2 Oct 2023", "Denied"),
+];
+
+
+const makeStyle=(status)=>{
+  if(status === 'Accept')
+  {
+    return {
+      background: 'rgb(145 254 159 / 47%)',
+      color: 'green',
+    }
+  }
+  else if(status === 'Denied')
+  {
+    return{
+      background: '#ffadad8f',
+      color: 'red',
+    }
+  }
+  else{
+    return{
+      background: '#59bfff',
+      color: 'white',
+    }
+  }
+}
+
+export default function BasicTable() {
+  return (
+      <div className="Table">
+      <h3>User</h3>
+        <TableContainer
+          component={Paper}
+          style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Username</TableCell>
+                <TableCell align="left">User ID</TableCell>
+                <TableCell align="left">Date</TableCell>
+                <TableCell align="left">Status</TableCell>
+                <TableCell align="left"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody style={{ color: "white" }}>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="left">{row.trackingId}</TableCell>
+                  <TableCell align="left">{row.date}</TableCell>
+                  <TableCell align="left">
+                    <span className="status" style={makeStyle(row.status)}>{row.status}</span>
+                  </TableCell>
+                  <TableCell align="left" className="Details">Details</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+  );
+}
